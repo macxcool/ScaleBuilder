36c8cc0dd730f7d9735d9ff8ff55ccd2a1b0bc36
Mark Coolen <mark.coolen@gmail.com>
2019-07-04 20:31:52 -0400

    Some work on the get-nextnote function.

--- ScaleBuilder.psm1
+++ ScaleBuilder.psm1
@@ -6,10 +6,10 @@ function get-scale {
         [ValidateSet('Major','Harmonic Minor','Natural Minor','Melodic Minor')] # could use a script to access the xml
         [string]$scaleType,
 
-        # Tonic note of scale
-        [Parameter(Mandatory=$true)]
-        [ValidatePattern('[a-fA-F][b#]{0,1}\d')]
-        [string]$tonicNote,
+        # Tonic note of scale with upper-case pitch, accidental and octave (e.g. Eb3)
+        [Parameter(Mandatory=$false)]
+        [ValidatePattern('[A-F][b#]{0,1}\d')]
+        [string]$tonicNote='C4',
 
         # number of octaves to span
         [Parameter(Mandatory=$false)]
@@ -18,11 +18,53 @@ function get-scale {
     )
     
     begin {
+        # Load the xml scale data into an object
         [xml]$scaleData = Get-Content -Path ./scales.xml
+        # Get the info that represents this scale type
+        $thisScale = $scaleData.SelectNodes('//Scale') | Where-Object -Property Name -eq $scaleType
     }
     
     process {
         
+    }
+    
+    end {
+    }
+}
+
+function get-nextnote {
+    [CmdletBinding()]
+    param (
+       # Initial note
+       [Parameter(Mandatory=$true)]
+       [String]$initialNote,
+
+       # Interval to next note
+       [Parameter(Mandatory=$true)]
+       [int]$interval
+    )
+    
+    begin {
+        
+        $intervals = @{
+            C=0;
+            D=2;
+            E=4;
+            F=5;
+            G=7;
+            A=9;
+            B=11
+        }
+    }
+    
+    process {
+        # Get the next letter name in the alphabet
+        $nextNoteLetter = [string][char]([int][char]$initialNote + 1) # only in Powershell ;-)
+        if ($nextNoteLetter -eq 'H') {
+            $nextNoteLetter -eq 'A'
+        }
+
+        # Determine if an accidental is needed
     }
     
     end {
